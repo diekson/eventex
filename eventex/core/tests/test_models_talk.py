a921fdc0975f440efe43a58c8bc8f8a2acbf21bc
Diekson Scardine <diekson@ymail.com>
2014-02-27 16:02:04 -0300

    Talk - fix template bug

--- eventex/core/tests/test_models_talk.py
+++ eventex/core/tests/test_models_talk.py
@@ -30,3 +30,30 @@ class TalkModelTest(TestCase):
     def test_period_manager(self):
         'Talk default manager must be instance of PreiodManager'
         self.assertIsInstance(Talk.objects, PeriodManager)
+
+class CourseModelTest(TestCase)
+    def setUp(self):
+        self.couse = Course.objects.create(
+                title       = u'Tutorial Django',
+                description = u'Descrição do Palestra',
+                start_time  = '10:00')
+                
+    def test_create(self):
+        'Talk instance should be saved'
+        self.assertEqual(1, self.talk.pk)
+        
+    def test_unicode(self):
+        'Talk string introduction should be de name.'
+        self.assertEqual(u'Introdução ao Django', unicode(self.talk))
+    
+    def test_speakers(self):
+        'Talk has many Speakers and vice-versa'
+        self.talk.speakers.create(
+                name='Henrique Bastos',
+                slug='henrique-bastos',
+                url='http://henriquebastos.net')
+        self.assertEqual(1, self.talk.speakers.count())
+
+    def test_period_manager(self):
+        'Talk default manager must be instance of PreiodManager'
+        self.assertIsInstance(Talk.objects, PeriodManager)
