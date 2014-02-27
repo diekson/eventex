a6ea15145912fd78d3103456cf8aa3a25297293b
Diekson Scardine <diekson@ymail.com>
2014-02-27 15:40:29 -0300

    Talk

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from eventex.core.managers import KindContactManager, PeriodManager
 
 class Speaker(models.Model):
     name = models.CharField(_('Nome'), max_length=255)
@@ -8,8 +9,16 @@ class Speaker(models.Model):
     url = models.URLField(_('Url'))
     description = models.TextField(_(u'Descrição'), blank=True)
     
+    class Meta:
+        verbose_name = _('palestrante')
+        verbose_name_plural = _('palestrantes')
+    
     def __unicode__(self):
         return self.name
+    
+    @models.permalink
+    def get_absolute_url(self):
+            return ('core:speaker_detail', (), {'slug': self.slug})
 
 class Contact(models.Model):
     KINDS = (
@@ -19,9 +28,33 @@ class Contact(models.Model):
     )
     
     speaker = models.ForeignKey('Speaker', verbose_name=_('palestrante'))
-    kind = models.CharField(_('tipo'),max_length=1, choices=KINDS)
-    value = models.CharField(_('valor'), max_length=255)    
+    kind    = models.CharField(_('tipo'),max_length=1, choices=KINDS)
+    value   = models.CharField(_('valor'), max_length=255)
     
+    objects = models.Manager()
+    emails  = KindContactManager('E')
+    phones  = KindContactManager('P')
+    faxes   = KindContactManager('F')    
     
     def __unicode__(self):
         return self.value
+
+class Talk(models.Model):
+    title   = models.CharField(_('Título'),max_length=200)
+    description = models.TextField(_('Descrição'))
+    start_time = models.TimeField(_('Horário'), blank=True)
+    speakers = models.ManyToManyField('Speaker', verbose_name=_('palestrantes'))
+    
+    objects = PeriodManager()
+    
+    class Meta:
+        verbose_name = _('palestra')
+        verbose_name_plural = _('palestras')
+    
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        #TODO: Use reverse.
+        
+        return '/palestras/%d/' % self.pk
---
+++ eventex/core/tests/test_models_talk.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from eventex.core.models import Talk
+from eventex.core.managers import PeriodManager
+
+class TalkModelTest(TestCase):
+    def setUp(self):
+        self.talk = Talk.objects.create(
+                title       = u'Introdução ao Django',
+                description = u'Descrição da Palestra',
+                start_time  = '10:00')
+                
+    def test_create(self):
+        'Talk instance should be saved'
+        self.assertEqual(1, self.talk.pk)
+        
+    def test_unicode(self):
+        'Talk string introduction should be de name.'
+        self.assertEqual(u'Introdução ao Django', unicode(self.talk))
+    
+    def test_speakers(self):
+        'Talk has many Speakers and vice-versa'
+        self.talk.speakers.create(
+                name='Henrique Bastos',
+                slug='henrique-bastos',
+                url='http://henriquebastos.net')
+        self.assertEqual(1, self.talk.speakers.count())
+
+    def test_period_manager(self):
+        'Talk default manager must be instance of PreiodManager'
+        self.assertIsInstance(Talk.objects, PeriodManager)
